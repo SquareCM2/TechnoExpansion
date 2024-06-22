11b2c7a65bc20769d07b58f475204d4b51f1a63d
SquareCM2 <155364934+SquareCM2@users.noreply.github.com>
2024-06-22 12:41:19 +0800

    更改基本信息

--- src/squarecm/TechnoExpansion.java
+++ src/squarecm/TechnoExpansion.java
@@ -1,17 +1,14 @@
-package example;
+package squarecm;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class TechnoExpansion extends Mod{
 
-    public ExampleJavaMod(){
+    public TechnoExpansion(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
